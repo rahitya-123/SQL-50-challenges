ae8f22a3be2cf4070ad0426ee3d30fd789f4028f
Rahitya Velpuri <33275106+rahitya-123@users.noreply.github.com>
2022-12-14 12:22:50 +0000

    Update customer-grouping.sql

--- customer-grouping.sql
+++ customer-grouping.sql
@@ -1,22 +1,37 @@
 USE AdventureWorks2012
 GO
 
-with customerTotalOrder_2013(CustomerId,SalesOrderID,TotalOrderAmount) AS
-(SELECT Customer.CustomerID
-	,SalesOrderHeader.SalesOrderID
-	,TotalOrderAmount = sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice)
-FROM sales.customer
-JOIN sales.salesorderheader ON Customer.CustomerID = SalesOrderHeader.CustomerID
-JOIN Sales.SalesOrderDetail ON SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
-WHERE year(OrderDate) = 2013
-GROUP BY customer.customerId
-	,SalesOrderHeader.SalesOrderID)
-select CustomerID, SalesOrderID, TotalOrderAmount,
-case
-    when 0 <= TotalOrderAmount AND TotalOrderAmount <= 1000 Then 'Low'
-    when 1000 < TotalOrderAmount AND TotalOrderAmount <= 5000 Then 'Medium'
-    when 5000 < TotalOrderAmount AND TotalOrderAmount <= 10000 Then 'High'
-    when TotalOrderAmount > 10000  Then 'Very High'
-end as CustomerGroup
-from customerTotalOrder_2013
-order by CustomerID;
+WITH customerTotalOrder_2013 (
+	CustomerId
+	,SalesOrderID
+	,TotalOrderAmount
+	)
+AS (
+	SELECT Customer.CustomerID
+		,SalesOrderHeader.SalesOrderID
+		,TotalOrderAmount = sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice)
+	FROM sales.customer
+	JOIN sales.salesorderheader ON Customer.CustomerID = SalesOrderHeader.CustomerID
+	JOIN Sales.SalesOrderDetail ON SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
+	WHERE year(OrderDate) = 2013
+	GROUP BY customer.customerId
+		,SalesOrderHeader.SalesOrderID
+	)
+SELECT CustomerID
+	,SalesOrderID
+	,TotalOrderAmount
+	,CASE 
+		WHEN 0 <= TotalOrderAmount
+			AND TotalOrderAmount <= 1000
+			THEN 'Low'
+		WHEN 1000 < TotalOrderAmount
+			AND TotalOrderAmount <= 5000
+			THEN 'Medium'
+		WHEN 5000 < TotalOrderAmount
+			AND TotalOrderAmount <= 10000
+			THEN 'High'
+		WHEN TotalOrderAmount > 10000
+			THEN 'Very High'
+		END AS CustomerGroup
+FROM customerTotalOrder_2013
+ORDER BY CustomerID;
