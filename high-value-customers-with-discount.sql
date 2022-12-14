4a9eeb4dbcbe70e25882a7caa4777c2f4e803e04
Rahitya Velpuri <33275106+rahitya-123@users.noreply.github.com>
2022-12-14 12:24:30 +0000

    Update high-value-customers-with-discount.sql

--- high-value-customers-with-discount.sql
+++ high-value-customers-with-discount.sql
@@ -3,36 +3,30 @@ GO
 
 SELECT Customer.customerId
 	,SalesOrderHeader.SalesOrderID
-	,totalOrderAmount = sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice*(1-SalesOrderDetail.UnitPriceDiscount))
+	,totalOrderAmount = sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice * (1 - SalesOrderDetail.UnitPriceDiscount))
 FROM sales.customer
 JOIN sales.salesorderheader ON Customer.CustomerID = SalesOrderHeader.CustomerID
 JOIN Sales.SalesOrderDetail ON SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
 WHERE year(OrderDate) = 2013
 GROUP BY customer.customerId
 	,SalesOrderHeader.SalesOrderID
-HAVING sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice*(1-SalesOrderDetail.UnitPriceDiscount)) > 10000
+HAVING sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice * (1 - SalesOrderDetail.UnitPriceDiscount)) > 10000
 ORDER BY totalOrderAmount;
 GO
 
 -- Using Window Functions
-
-Select customerid,totalswithoutdiscount,totalswithdiscount
-from 
-(select distinct Customer.customerId,
-sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice*(1-SalesOrderDetail.UnitPriceDiscount)) 
-over(partition by Customer.customerId)as totalswithdiscount,
-sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice) 
-over(partition by Customer.customerId) as totalswithoutdiscount
-
-FROM sales.customer
-JOIN sales.salesorderheader ON Customer.CustomerID = SalesOrderHeader.CustomerID
-JOIN Sales.SalesOrderDetail ON SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
-WHERE year(OrderDate) = 2013
-) as T
-where totalswithdiscount>10000;
-Go
-
-
-
-
+SELECT customerid
+	,totalswithoutdiscount
+	,totalswithdiscount
+FROM (
+	SELECT DISTINCT Customer.customerId
+		,sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice * (1 - SalesOrderDetail.UnitPriceDiscount)) OVER (PARTITION BY Customer.customerId) AS totalswithdiscount
+		,sum(SalesOrderDetail.orderqty * SalesOrderDetail.unitprice) OVER (PARTITION BY Customer.customerId) AS totalswithoutdiscount
+	FROM sales.customer
+	JOIN sales.salesorderheader ON Customer.CustomerID = SalesOrderHeader.CustomerID
+	JOIN Sales.SalesOrderDetail ON SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
+	WHERE year(OrderDate) = 2013
+	) AS T
+WHERE totalswithdiscount > 10000;
+GO
 
