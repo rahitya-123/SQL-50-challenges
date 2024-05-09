7232a8a3fbe390bc1d9bb5f336e5354940815bee
Rahitya Velpuri <33275106+rahitya-123@users.noreply.github.com>
2024-05-09 12:54:16 +0530

    Update Highest-Grossing-Items.sql

--- Highest-Grossing-Items.sql
+++ Highest-Grossing-Items.sql
@@ -10,7 +10,7 @@ category			string
 product				string
 user_id				integer
 spend				decimal
-transaction_date	timestamp
+transaction_date	        timestamp
 
 
 product_spend Example Input:
@@ -18,17 +18,17 @@ category	product					user_id	spend	transaction_date
 appliance	refrigerator				165	246.00	12/26/2021 12:00:00
 appliance	refrigerator				123	299.99	03/02/2022 12:00:00
 appliance	washing machine				123	219.80	03/02/2022 12:00:00
-electronics	vacuum						178	152.00	04/05/2022 12:00:00
+electronics	vacuum					178	152.00	04/05/2022 12:00:00
 electronics	wireless headset			156	249.90	07/08/2022 12:00:00
-electronics	vacuum						145	189.00	07/15/2022 12:00:00
+electronics	vacuum					145	189.00	07/15/2022 12:00:00
 
 
 Example Output:
 category	product				total_spend
-appliance	refrigerator		299.99
-appliance	washing machine		219.80
+appliance	refrigerator			299.99
+appliance	washing machine			219.80
 electronics	vacuum				341.00
-electronics	wireless headset	249.90
+electronics	wireless headset		249.90
 
 Explanation:
 Within the "appliance" category, the top two highest-grossing products are "refrigerator" and "washing machine."
